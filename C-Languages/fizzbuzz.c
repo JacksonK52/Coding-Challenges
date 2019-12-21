91111f5597abf762e9f4c45d07b665f9c95cb5e2
JacksonK52 <konjack71@yahoo.in>
2019-12-21 13:57:28 +0530

    Modifying code

--- C-Languages/fizzbuzz.c
+++ C-Languages/fizzbuzz.c
@@ -6,20 +6,15 @@ void fizzbuzz()
         if(i == 0)
             printf("%d, ", i);
         else if((i%3) == 0)
-        {
             printf("Fizz, ");
-        }
         else if((i%5) == 0)
-        {
             printf("Buzz, ");
-        }
         else
-        {
             printf("%d, ",i);
-        }
     }
     printf("\n");
 }
+
 void main()
 {
     printf("==============================\n");
